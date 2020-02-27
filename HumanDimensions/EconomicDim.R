9540da0c8de5bd41f9306391acb53123ddfa1075
XanderHuggins <xander.huggins@gmail.com>
2020-02-26 17:53:02 -0800

    Update EconomicDim.R

--- HumanDimensions/EconomicDim.R
+++ HumanDimensions/EconomicDim.R
@@ -4,7 +4,7 @@ library(e1071); library(reshape2);library(spatstat); library(RColorBrewer)
 mainDir <- "Z:/2.active_projects/Xander/"
 
 # import all data
-TWS_trend <- raster(paste(mainDir, "! GIS_files/GRACE/", "GRACE_coredata", ".tif", sep="")) # Rodell et al. (2018) TWS trends, resampled to 0.05 res
+TWS_trend <- raster(paste(mainDir, "! GIS_files/Rodell_SourceData/", "Rodell_etal_0d05", ".tif", sep="")) # Rodell et al. (2018) TWS trends
 GDP_ppp <- raster(paste(mainDir, "! GIS_files/GDP/Kummu/", "GDP_PPP_2015_0d05", ".tif", sep="")) # Kummu et al. (2019) GDP (PPP) dataset for 2015
 GDPpc <- raster(paste(mainDir, "! GIS_files/GDP/Kummu/", "GDPpc_2015_0d05", ".tif", sep="")) # Kummu et al. (2019) GDP (PPP) dataset for 2015
 Pop <- raster(paste(mainDir, "! GIS_files/R_gis_exports/", "POP_2015_0d05_UNWPP", ".tif", sep="")) # GWPv4 UNWPP 2015 population dataset 
@@ -32,7 +32,6 @@ GDP.veryhigh[GDPpc_IMFclass == 5]     <- GDP_ppp[GDPpc_IMFclass == 5]
 GDP.NoDat[GDPpc_IMFclass !=  1 & GDPpc_IMFclass !=  2 & GDPpc_IMFclass !=  3 & GDPpc_IMFclass !=  4 & GDPpc_IMFclass !=  5] <-
   GDP_ppp[GDPpc_IMFclass !=  1 & GDPpc_IMFclass !=  2 & GDPpc_IMFclass !=  3 & GDPpc_IMFclass !=  4 & GDPpc_IMFclass !=  5]
 
-
 # Reclassify emerging trend into 0.1 cm/yr increment bins
 ReclassRanges <- data.frame(low = seq(-40, 6.0, 0.1), high = seq(-39.9, 6.1, 0.1), ReCLASS = seq(1:461))
 EmergingTrend_Reclass <- reclassify(TWS_trend, ReclassRanges)
@@ -50,31 +49,33 @@ GDP.distr <- Reduce(function(x, y) merge(x, y, by = "ID"), list(GDP.verylow.dist
                                                                 GDP.high.distr, GDP.veryhigh.distr, GDP.NoDat.distr))
 GDP.distr %<>% melt(id.var = "ID")
 
-## plot histogram results
-# For reference: bin ID vs emerging trend:
-# 390.5 = -1; 400.5 = 0; 410.5 = 1
-hist <- ggplot(GDP.distr, aes(x = ID, y = value, fill = variable)) +
+# plotting variables
+a <- 0.25
+b <- 9.5*1e12
+
+fig <- ggplot(GDP.distr, aes(x = ID, y = value, fill = variable)) +
+  geom_rect(data=NULL,aes(xmin=350.5, xmax=380.5, ymin=0, ymax=b), fill="#FFA5A5", alpha = a) +
+  geom_rect(data=NULL,aes(xmin=380.5, xmax=395.5, ymin=0, ymax=b), fill="#FFDFDF", alpha = a) +
+  geom_rect(data=NULL,aes(xmin=395.5, xmax=405.5, ymin=0, ymax=b), fill="#959595", alpha = 0.1*a) +
+  geom_rect(data=NULL,aes(xmin=405.5, xmax=420.5, ymin=0, ymax=b), fill="#F0EDF8", alpha = a) +
+  geom_rect(data=NULL,aes(xmin=420.5, xmax=430.5, ymin=0, ymax=b), fill="#AAC2FF", alpha = a) +
   geom_bar(stat = "identity", position="stack", colour = "black") +
   scale_fill_manual(values = c("#bc2d18", "#d66a22", "#f2bc8f", "#9cd1bb", "#46a7af", "#d5d7da")) +
   scale_y_continuous(position = "right", breaks = seq(0, 1e13, by = 1e12), expand = c(0,0)) +
-  scale_x_continuous(limits = c(350, 431), breaks = seq(350.5, 430.5, by = 10), expand = c(0,0),  
+  scale_x_continuous(limits = c(350.5, 430.5), breaks = seq(350.5, 430.5, by = 10), expand = c(0,0),  
                      labels = c("-5", "-4", "-3", "-2", "-1", "0", "1", "2", "3")) +
-  theme(axis.title.x = element_blank(), axis.title.y = element_blank(),
-        panel.border = element_rect(colour = "black", fill=NA, size= 1.5),
-        axis.title = element_text(size = 11, color = "black"),
-        panel.background = element_rect(fill = "transparent", colour = NA),
+  theme(panel.background =  element_rect(fill = "transparent"),
         plot.background = element_rect(fill = "transparent", colour = NA),
-        panel.grid.major.x = element_line(alpha(colour = "white"), linetype = "dashed", size = 0.5),
         panel.grid.minor.x = element_blank(), 
         panel.grid.minor.y = element_blank(),
-        panel.grid.major.y = element_line(alpha(colour = "white"), linetype = "dashed", size = 0.5),
-        axis.line = element_line(colour = "black"), 
-        axis.text = element_text(face = "bold", size = 10, color = "black"),
+        panel.grid.major = element_line(alpha(colour = "white", 0.3), linetype = "dashed", size = 0.5),
+        axis.line = element_line(colour = "black"), panel.ontop = TRUE,
         legend.position = "none") +
-  geom_vline(xintercept = 400.5, size = 1.5)
-hist
+  geom_vline(xintercept = 400.5, size = 1.5) +
+  labs(x=expression(paste("Terrestrial Water Storage Rate of Change","  ","(cm", y^-1,")", sep="")),
+       y = "Global Food Production kcal (trillions)\n") 
+fig; # ggsave("C:/Users/Tom/Desktop/GDP_distribution.png", fig, dpi = 500, width = 14, height = 10, units = "in", bg = "transparent") 
 
-ggsave("C:/Users/Tom/Desktop/GDP_hist.png", hist, dpi = 500, width = 14, height = 10, units = "in", bg = "transparent")
 
 ##################
 ## cdf analysis ##
@@ -97,7 +98,7 @@ cdf <- ggplot(Combined_df, aes(x= ID)) +
   geom_line(aes(y = POP_cdf), colour = "red", lwd = 0.75) + 
   geom_line(aes(y = GDP_cdf), colour = "blue", lwd = 0.75) +
   scale_y_continuous(position = "right", breaks = seq(0, 1, by = 0.1), expand = c(0,0)) +
-  scale_x_continuous(limits = c(350, 431), breaks = seq(350.5, 430.5, by = 10), expand = c(0,0),  
+  scale_x_continuous(limits = c(350.5, 430.5), breaks = seq(350.5, 430.5, by = 10), expand = c(0,0),  
                      labels = c("-5", "-4", "-3", "-2", "-1", "0", "1", "2", "3")) +
   theme(legend.position = "none",
         axis.title.x = element_blank(), axis.title.y = element_blank(),
@@ -110,6 +111,4 @@ cdf <- ggplot(Combined_df, aes(x= ID)) +
         axis.line.x = element_blank(),
         axis.ticks = element_blank(),
         plot.margin=grid::unit(c(0,0,0,0), "mm")) 
-cdf
-
-ggsave("C:/Users/Tom/Desktop/GDP_cdf.png", cdf, dpi = 500, width = 3.615, height = 1.815, units = "in", bg = "transparent")
+cdf; # ggsave("C:/Users/Tom/Desktop/GDP_cdf.png", cdf, dpi = 500, width = 3.615, height = 1.815, units = "in", bg = "transparent")
