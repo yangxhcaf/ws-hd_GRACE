5b1469544c1e94b28697c8fe7621e55a74652d4a
XanderHuggins <xander.huggins@gmail.com>
2020-05-14 16:39:18 -0400

    Create Calories_in_shortage.R

---
+++ AdditionalResources/Calories_in_shortage.R
@@ -0,0 +1,58 @@
+library(ggplot2); library(raster); library(dplyr); library(magrittr) 
+library(reshape2); library(spatstat); library(e1071); library(rgdal); library(gdalUtils)
+library(tmap); library(tmaptools)
+if(!is.null(dev.list())) dev.off(); cat("\014");rm(list=ls())
+setwd("Z:/2.active_projects/Xander/! GIS_files")
+
+# import all data
+TWS_trend <- raster("./Rodell_SourceData/Rodell_etal_0d05.tif") # Rodell et al. (2018) TWS trends, resampled to 0.05 res
+GridArea <- raster("./R_gis_exports/WGS84_cellArea_0d05res.tif") # Grid area estimate for WGS84 reference elipsoid
+FEED <- raster("./CropAllocationFoodFeedFuel_Geotiff/Cassidy_gs/FEED_0d05_gs.tif")
+FOOD <- raster("./CropAllocationFoodFeedFuel_Geotiff/Cassidy_gs/FOOD_0d05_gs.tif")
+NONFOOD <- raster("./CropAllocationFoodFeedFuel_Geotiff/Cassidy_gs/NONFOOD_0d05_gs.tif")
+All_kcal <- FEED + FOOD + NONFOOD # sum of all kcal sets
+All_kcal_r <- All_kcal
+WaterShortage <- raster("./WaterScarcityAtlas/Shortage_2001t2010_Kummu_0d05.tif", sep="") # Kummu et al water crowding data
+
+EQ_regions <- raster("./Earthquakes/EQ_rm_0d05.tif") # earthquake regions to remove
+
+# Remove EQ regions from input data
+TWS_trend[EQ_regions[] == 1] <- NA
+FEED[EQ_regions[] == 1] <- NA
+FOOD[EQ_regions[] == 1] <- NA
+NONFOOD[EQ_regions[] == 1] <- NA
+All_kcal[EQ_regions[] == 1] <- NA
+WaterShortage[EQ_regions[] == 1] <- NA
+
+# Set NAs to -99 for zonal statistics
+WaterShortage_id <- raster(TWS_trend)
+WaterShortage_id[is.na(WaterShortage[])] <- -1 # index for no data
+WaterShortage_id[WaterShortage <= 1e6/2000] <- 5 # Falkenmark stress level 5
+WaterShortage_id[WaterShortage > 1e6/2000 & WaterShortage <= 1e6/1000] <- 4 # Falkenmark stress level 4
+WaterShortage_id[WaterShortage > 1e6/1000 & WaterShortage <= 1e6/600] <- 3 # Falkenmark stress level 3
+WaterShortage_id[WaterShortage > 1e6/600 & WaterShortage <= 1e6/100] <- 2 # Falkenmark stress level 2
+WaterShortage_id[WaterShortage > 1e6/100] <- 1 # Falkenmark stress level 1
+
+TWS_trend_id <- raster(TWS_trend)
+TWS_trend_id[TWS_trend <= -2] <- 1
+TWS_trend_id[TWS_trend >-2 & TWS_trend <= -0.5] <- 2
+TWS_trend_id[TWS_trend >-0.5 & TWS_trend < 0.5] <- 3
+TWS_trend_id[TWS_trend >= 0.5 & TWS_trend < 2] <- 4
+TWS_trend_id[TWS_trend >= 2] <- 5
+
+df <- cbind(as.data.frame(TWS_trend_id), as.data.frame(FEED), as.data.frame(FOOD),
+            as.data.frame(NONFOOD), as.data.frame(WaterShortage_id)) %>% 
+  set_colnames(c("TWS.id", "FEED", "FOOD", "NONFOOD", "Shortage.id"))
+
+df$TWS.id %<>% as.factor()
+df$Shortage.id %<>% as.factor()
+df_s <- df[complete.cases(df$NONFOOD), ]
+
+# summary table per TWS trend class (5 classes) and water stress class (7 classes) = 35 population counts (in millions)
+Ag_summary <- df_s %>% group_by(TWS.id, Shortage.id) %>% 
+  summarise(FOOD_billion = sum(FOOD, na.rm = T)/1e9,
+            FEED_billion = sum(FEED, na.rm = T)/1e9,
+            NONFOOD_billion = sum(NONFOOD, na.rm = T)/1e9) %>% as.data.frame()
+
+write.csv(Ag_summary,"C:/Users/Tom/Desktop/kcal_per_WaterShortage.csv", 
+          row.names = FALSE)
