c81818b5944fc980db4d95d892a10fcd58da3bce
XanderHuggins <xander.huggins@gmail.com>
2020-05-14 13:56:41 -0400

    Update and rename DataPrep/WGS84_gridArea_cellcenter.R to Data preparation/WGS84_cellArea.R

--- Data preparation/WGS84_cellArea.R
+++ Data preparation/WGS84_cellArea.R
@@ -1,6 +1,7 @@
-library(raster); library(dplyr); library(magrittr)
+# this script creates a raster with cell values corresponding to surface area
+# based on the WGS 84 reference ellipsoid at any desired resolution (smaller resolutions provide more accurate area estimates)
 
-# this script creates a raster to correct for pixel area differences at specified degree resolution
+library(raster); library(dplyr); library(magrittr)
 a <- 6378137.0  # Semi-major Axis 
 b <- 6356752.3142 # Semi-minor Axis 
 e2 <- 0.00669437999014 # First Eccentricity Squared 
@@ -13,6 +14,7 @@ RES <- 0.5
 LatDegrees <- c(seq(90-(RES/2), -90+(RES/2), by = -RES)) %>% as.data.frame()
 colnames(LatDegrees) <- "LAT"
 
+# create dataframe with width and height estimates per grid cell at each latitude interval
 WGSgridAreas <- LatDegrees
 WGSgridAreas$LAT_in_rad <- pi*WGSgridAreas$LAT/180
 WGSgridAreas$dLONG_m <- (RES)*((pi*a*cos(WGSgridAreas$LAT_in_rad))/(180*sqrt(1-(e2*(sin(WGSgridAreas$LAT_in_rad)^2)))))
@@ -21,13 +23,14 @@ WGSgridAreas$dLAT_m <- (RES)*((pi*a*(1-e2))/(180*((1-(e2*sin(WGSgridAreas$LAT_in
 WGSgridAreas$dLAT_km <- WGSgridAreas$dLAT_m/1e3
 WGSgridAreas$Area_km2 <- WGSgridAreas$dLAT_km * WGSgridAreas$dLONG_km
 
+# turn dataframe into raster
 WGS84area <- as.vector(WGSgridAreas$Area_km2)
 WGS84area.matrix <- matrix(WGS84area, nrow = 180*(1/RES), ncol = 360*(1/RES), byrow = FALSE, dimnames = NULL)
 WGS84area_ras <- raster(WGS84area.matrix)
 extent(WGS84area_ras) <- c(-180, 180, -90, 90)
 crs(WGS84area_ras) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
 
-sum(WGS84area_ras[])
+sum(WGS84area_ras[]) # check global surface area estimate
 
 # Write raster - manually change filename
 writeRaster(WGS84area_ras, 
