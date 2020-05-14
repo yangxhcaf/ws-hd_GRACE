3cbbd1d5fe203116077c9e96ae7c16da1afb0a98
XanderHuggins <xander.huggins@gmail.com>
2020-05-14 14:47:00 -0400

    Update and rename DataPrep/kcal_resampling.R to Data preparation/kcal_resampling.R

--- Data preparation/kcal_resampling.R
+++ Data preparation/kcal_resampling.R
@@ -1,17 +1,16 @@
-library(ggplot2); library(raster); library(dplyr); library(magrittr)
-library(reshape2); library(spatstat); library(e1071); library(rgdal); library(sf)
-library(tmap); library(tmaptools)
+# this script resamples the Cassidy et al. calorie data product to 0.05d resolution while preserving global sum
+
+library(raster); library(dplyr); library(magrittr)
 if(!is.null(dev.list())) dev.off(); cat("\014");rm(list=ls())
+setwd("Z:/2.active_projects/Xander/! GIS_files/")
 
-mainDir <- "Z:/2.active_projects/Xander/! GIS_files/"
+# import grid area raster at 0.05d for resolution reference
+GridArea <- raster("./R_gis_exports/WGS84_cellArea_0d05res.tif") 
 
-# import grid area raster already in 0.05 resolution
-GridArea <- raster(paste(mainDir, "R_gis_exports/", "WGS84_cellArea_0d05res", ".tif", sep="")) # Grid area estimate for WGS84 reference elipsoid
-
-# import raw feed, food, and nonfood rasters at 0.083333 resolution
-FEED <- raster(paste(mainDir, "CropAllocationFoodFeedFuel_Geotiff/Cassidy_etal/", "GlbFeedkcal", ".tif", sep=""))
-FOOD <- raster(paste(mainDir, "CropAllocationFoodFeedFuel_Geotiff/Cassidy_etal/", "GlbFoodkcal", ".tif", sep=""))
-NONFOOD <- raster(paste(mainDir, "CropAllocationFoodFeedFuel_Geotiff/Cassidy_etal/", "GlbNonFoodkcal", ".tif", sep=""))
+# import raw feed, food, and nonfood calorie rasters at 0.083333 resolution
+FEED <- raster("./CropAllocationFoodFeedFuel_Geotiff/Cassidy_etal/GlbFeedkcal.tif")
+FOOD <- raster("./CropAllocationFoodFeedFuel_Geotiff/Cassidy_etal/GlbFoodkcal.tif")
+NONFOOD <- raster("./CropAllocationFoodFeedFuel_Geotiff/Cassidy_etal/GlbNonFoodkcal.tif")
 
 # aggregate each Cassidy dataset to 0.25 resolution (to preserve global sum); lowest aggregation factor that is also multiple of 0.05
 FEED_0d25agg <- raster::aggregate(FEED, fact = (0.25/res(FEED)), fun = sum)
@@ -27,5 +26,6 @@ FEED_0d05_gs <- FEED_0d05/25
 FOOD_0d05_gs <- FOOD_0d05/25
 NONFOOD_0d05_gs <- NONFOOD_0d05/25
 
+# write each raster individually ... 
 writeRaster(NONFOOD_0d05_gs, filename=paste(mainDir, "CropAllocationFoodFeedFuel_Geotiff/Cassidy_gs/", "NONFOOD_0d05_gs", ".tif", sep=""),
             format="GTiff", overwrite=TRUE)
